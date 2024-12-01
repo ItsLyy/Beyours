ebd0e1408fd80671ade5a883e61d86cf8785ecee
ItsLyy <irlyfizaharis@gmail.com>
2024-12-01 21:44:39 +0700

    Fix bug Sidebar UI

--- resources/js/Components/Dashboard/NavigationButton.jsx
+++ resources/js/Components/Dashboard/NavigationButton.jsx
@@ -2,7 +2,7 @@ import { Link } from "@inertiajs/react";
 
 export default function NavigationButton({ children, active = false, ...props  }) {
   return (
-  <Link {...props} className={"flex items-center gap-6 text-beyours-250 font-thin md:w-full p-4 box-border text-sm md:text-base " + (active ? "!text-white stroke-white" : "")}>
+  <Link {...props} className={"flex items-center gap-6 text-beyours-250 font-thin md:w-full p-4 box-border text-sm md:text-base hover:text-beyours-100 hover:stroke-beyours-100 hover:bg-beyours-600 rounded-sm transition-all ease-in-out duration-300 " + (active ? "!text-white stroke-white" : "")}>
       {children}
     </Link>
   );
